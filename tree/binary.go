49f7a461a70036d6d852f1cc64e9c56ff980ff7b
B.G.Adrian <btools@gmail.com>
2017-09-13 22:19:35 +0300

    BST working with tests&example

--- tree/binary.go
+++ tree/binary.go
@@ -1,10 +1,11 @@
+/*Package tree contains simple Tree implementations for academic purposes.*/
 package tree
 
 import (
 	"errors"
 )
 
-//Node ...
+//Node An element of the tree.
 type Node struct {
 	K     int
 	V     interface{}
@@ -12,19 +13,29 @@ type Node struct {
 	Right *Node
 }
 
-// func (n *Node) String() string {
-// 	return string(n.K)
-// }
-
-//BinarySearch ...
-type BinarySearch struct {
+//BST Binary Search Tree simple implementation.
+//it does NOT have auto balance algorithm.
+type BST struct {
 	Root *Node
 	n    int
 	h    int
 }
 
-//Insert ...
-func (b *BinarySearch) Insert(key int, value interface{}) (n *Node, err error) {
+//InsertKeys Add multiple elements. Doesn't support adding values, only keys.
+func (b *BST) InsertKeys(keys ...int) (int, error) {
+	i := 0
+	for _, k := range keys {
+		_, err := b.Insert(k, nil)
+		if err != nil {
+			return i, err
+		}
+		i++
+	}
+	return i, nil
+}
+
+//Insert Add a new element.
+func (b *BST) Insert(key int, value interface{}) (n *Node, err error) {
 
 	if b.Root == nil {
 		n = &Node{K: key, V: value}
@@ -61,22 +72,126 @@ func (b *BinarySearch) Insert(key int, value interface{}) (n *Node, err error) {
 
 	*l = &Node{K: key, V: value}
 	n = *l
+	b.n++
 
 	return
 }
 
-//Search ...
-func (b *BinarySearch) Search(key int) (n *Node) {
+//Search Find a node with a specific key.
+func (b *BST) Search(key int) (n *Node) {
+	node, _ := b.SearchParent(key)
+	return node
+}
+
+//SearchParent Return the node with a specific key, and it's parent.
+func (b *BST) SearchParent(key int) (n *Node, p *Node) {
 	for c := b.Root; c != nil && n == nil; {
 		if c.K == key {
 			n = c
 		} else if key < c.K {
+			p = c
 			c = c.Left
 		} else {
+			p = c
 			c = c.Right
 		}
 	}
 	return
+}
+
+//Remove Delete an element with a specific key.
+func (b *BST) Remove(key int) error {
+	if b.Root == nil {
+		return errors.New("tree is empty")
+	}
+
+	var r error
+
+	if b.Root.K == key {
+		auxRoot := &Node{}
+		auxRoot.Left = b.Root
+		r = b.Root.remove(key, auxRoot)
+		b.Root = auxRoot.Left
+	} else {
+		r = b.Root.remove(key, nil)
+	}
+
+	if r == nil {
+		b.n--
+	}
+	return r
+
+}
+
+func (n *Node) remove(key int, parent *Node) error {
+	if key < n.K {
+		if n.Left == nil {
+			return errors.New("cannot find key")
+		}
+		return n.Left.remove(key, n)
+	}
+
+	if key > n.K {
+		if n.Right == nil {
+			return errors.New("cannot find key")
+		}
+		return n.Right.remove(key, n)
+	}
+
+	if n.Left != nil && n.Right != nil {
+		min := n.Right.minNode()
+		n.K = min.K
+		n.V = min.V
+		return n.Right.remove(n.K, n)
+	}
+
+	if parent.Left == n {
+		if n.Left == nil {
+			parent.Left = n.Right
+		} else {
+			parent.Left = n.Left
+		}
+		return nil
+	}
+
+	if parent.Right == n {
+		if n.Left == nil {
+			parent.Right = n.Right
+		} else {
+			parent.Right = n.Left
+		}
+	}
+	return nil
+}
+
+func (n *Node) minNode() *Node {
+	if n.Left == nil {
+		return n
+	}
+	return n.Left.minNode()
+}
+
+//Size How many elements are now.
+func (b *BST) Size() int {
+	return b.n
+}
+
+//Inorder Get the Inorder list of keys.
+func (b *BST) Inorder() []int {
+	var traverse func(n *Node, r []int, i *int)
+	traverse = func(n *Node, r []int, i *int) {
+		if n == nil {
+			return
+		}
+		traverse(n.Left, r, i)
+		r[*i] = n.K
+		(*i)++
+		traverse(n.Right, r, i)
+	}
+	arr := make([]int, b.n)
+	i := 0
+	traverse(b.Root, arr, &i)
+	return arr
 }
 
 //Height ...
--- tree/binary_test.go
+++ tree/binary_test.go
@@ -1,9 +1,14 @@
 package tree
 
 import "testing"
+import "sort"
+import "math/rand"
 
-func TestBinaryBasic(t *testing.T) {
-	b := BinarySearch{}
+import "fmt"
+import "strconv"
+
+func TestBSTBasic(t *testing.T) {
+	b := BST{}
 
 	n, err := b.Insert(42, nil)
 	quickAssertErr(err, t)
@@ -24,8 +29,8 @@ func TestBinaryBasic(t *testing.T) {
 	}
 }
 
-func TestBinaryInsertOrder(t *testing.T) {
-	b := BinarySearch{}
+func TestBSTInsertOrder(t *testing.T) {
+	b := BST{}
 
 	var r, rl, rr *Node          //first level of the tree
 	var rll, rlr, rrl, rrr *Node //2nd level
@@ -36,6 +41,8 @@ func TestBinaryInsertOrder(t *testing.T) {
 				30				50
 			20		35		46		60
 		10		25
+
+		inorder: 10,20,25,30,35,42,46,50,60
 	*/
 
 	r, _ = b.Insert(42, nil)
@@ -63,8 +70,8 @@ func TestBinaryInsertOrder(t *testing.T) {
 	quickAssert(b.Root.Left.Left.Right, rllr, "r L L R", t)
 }
 
-func TestBinarySearch(t *testing.T) {
-	b := BinarySearch{}
+func TestBSTInOrder(t *testing.T) {
+	b := BST{}
 
 	b.Insert(42, nil)
 	b.Insert(30, nil)
@@ -72,6 +79,20 @@ func TestBinarySearch(t *testing.T) {
 	b.Insert(20, nil)
 	b.Insert(35, nil)
 	b.Insert(46, nil)
+	b.Insert(60, nil)
+	b.Insert(10, nil)
+	b.Insert(25, nil)
+
+	should := []int{10, 20, 25, 30, 35, 42, 46, 50, 60}
+	got := b.Inorder()
+
+	quickAssertArrInt(should, got, fmt.Sprintf("inorder exp %v, got %v", should, got), t)
+}
+
+func TestBSTSearch(t *testing.T) {
+	b := BST{}
+
+	b.InsertKeys(42, 30, 50, 20, 35, 46)
 
 	n := b.Search(42)
 	if n == nil {
@@ -85,8 +106,116 @@ func TestBinarySearch(t *testing.T) {
 
 	n = b.Search(99)
 	if n != nil {
-		t.Error("found a missing node ?!")
+		t.Error("found a ghost node ?!")
 	}
+}
+
+func TestBSTRemoveAndSize(t *testing.T) {
+	b := BST{}
+
+	err := b.Remove(42)
+	if err == nil {
+		t.Error("removing an existing node didn't returned error")
+	}
+
+	b.Insert(42, nil)
+	quickAssertInt(1, b.Size(), "1 size after add root", t)
+
+	err = b.Remove(42)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if b.Root != nil {
+		t.Errorf("removing the root, only node didn't worked, %+v", b.Root)
+	}
+
+	quickAssertInt(0, b.Size(), "0 size after remove root", t)
+
+	var count int
+	count, err = b.InsertKeys(3, 1, 2, 3)
+
+	if err == nil {
+		t.Error("InsertKeys duplicate key didn't returned error")
+	}
+
+	quickAssertInt(3, count, "InsertKeys returned wrong value", t)
+
+	b.Remove(3)
+	quickAssertArrInt([]int{1, 2}, b.Inorder(), "after remove root with 2 kids", t)
+	b.Remove(1)
+	quickAssertArrInt([]int{2}, b.Inorder(), "after remove root with 1 kids", t)
+	b.Remove(2)
+
+	//remove not existing keys
+	b.InsertKeys(2, 1, 3)
+	err = b.Remove(0)
+	if err == nil {
+		t.Error("not existing key remove should return error")
+	}
+	err = b.Remove(4)
+	if err == nil {
+		t.Error("not existing key remove should return error")
+	}
+}
+
+func TestBSTRemoveTable(t *testing.T) {
+	table := [][]int{
+		{5, 1, 3},
+		{5, 1, 3, 9, 10, 2, 4, 11},
+		{42, 30, 11, 23, 50, 60, 70, 33, 90, 21, 99},
+		{1, 2, 3, 4, 5, 6, 7, 8},
+		{1, 2, 3, 4, 5, 6, 7, 8, 0, -1, -2, -3, -4},
+		{40, 20, 60, 10, 30, 50, 70},
+		{40, 20, 60, 10, 30, 50, 70, 25, 35, 15, 45, 55, 65},
+		{9, 8, 7, 6, 5, 4, 3},
+		{20, 19, 21, 22, 18, 23, 17, 16, 15, 24, 25, 26},
+		{20, 19, 21, 22, 18, 23, 17, 16, 15, 24, 25, 26, 1, 2, 3, 4, 5, 6},
+	}
+
+	for _, v := range table {
+		bstRemoveAutoTest(v, t)
+	}
+}
+
+func bstRemoveAutoTest(k []int, t *testing.T) {
+	b := BST{}
+	b.InsertKeys(k...)
+
+	rand.Seed(1)
+
+	//choose a random value and remove it from both lists
+	removeValue := func(slice []int, v int) []int {
+		s := 0
+		for i, val := range slice {
+			if val == v {
+				s = i
+				break
+			}
+		}
+		return append(slice[:s], slice[s+1:]...)
+	}
+
+	//sort the entries, so we can easily compare with
+	//the inorder array
+	sort.Ints(k)
+
+	for b.n > 0 {
+		val := k[rand.Intn(b.n)]
+		k = removeValue(k, val)
+
+		b.Remove(val)
+		got := b.Inorder()
+
+		quickAssertArrInt(k, got, "fail after removing "+strconv.Itoa(val), t)
+	}
+}
+
+func quickAssertStr(exp, got string, m string, t *testing.T) {
+	if exp == got {
+		return
+	}
+	t.Errorf("exp %v, got %v : %v", exp, got, m)
 }
 
 func quickAssert(exp, got *Node, m string, t *testing.T) {
@@ -108,4 +237,43 @@ func quickAssertErr(err error, t *testing.T) {
 		return
 	}
 	t.Error(err)
+}
+
+func quickAssertArrInt(exp, got []int, m string, t *testing.T) {
+	if len(exp) != len(got) {
+		t.Error(m)
+		return
+	}
+
+	for i, v := range exp {
+		if v != got[i] {
+			t.Error(m)
+			return
+		}
+	}
+}
+
+func ExampleBST() {
+	b := BST{}
+
+	_, err := b.Insert(42, nil)
+
+	if err != nil {
+		//do something
+	}
+
+	b.InsertKeys(30, 50, 24, 60)
+	fmt.Printf("elements in order:%+v\n", b.Inorder())
+
+	b.Remove(50)
+	fmt.Printf("elements in order:%+v\n", b.Inorder())
+
+	fmt.Printf("root is:%v\n", b.Root.K)
+	fmt.Printf("left of root is:%v\n", b.Root.Left.K)
+
+	// Output:
+	//elements in order:[24 30 42 50 60]
+	//elements in order:[24 30 42 60]
+	//root is:42
+	//left of root is:30
 }
